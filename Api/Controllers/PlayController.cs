4b56c90be8438b14099d12c4467ec20ba13717d3
mustafaguler3 <mustafa.glr@hotmail.com>
2023-08-19 21:37:35 +0300

    added play controller for jwt interceptor in client app

---
+++ Api/Controllers/PlayController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class PlayController : ControllerBase
+	{
+		[HttpGet("get-players")]
+		public IActionResult Players()
+		{
+			return Ok(new JsonResult(new { message = "only authorized users an view players" }));
+		}
+	}
+}
+
