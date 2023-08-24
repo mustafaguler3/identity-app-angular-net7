711a9fddaf9bbe8cef06f4c89ba86e97e33184ae
mustafaguler3 <mustafa.glr@hotmail.com>
2023-08-24 14:17:22 +0300

    .

--- Api/Controllers/AccountController.cs
+++ Api/Controllers/AccountController.cs
@@ -211,6 +211,11 @@ namespace Api.Controllers
         {
             var user = await _userManager.FindByNameAsync(User.FindFirst(ClaimTypes.Email)?.Value);
 
+            if(await _userManager.IsLockedOutAsync(user))
+            {
+                return Unauthorized("You have been locked out");
+            }
+
             return await createAppUserDto(user);
         }
 
